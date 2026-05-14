53cbb9edeb07b689615fede924b3667a1e947092
wohara224 <wohara@youtheng.co.jp>
2026-05-14 11:28:02 +0900

    演習2実行

--- QForm/Pages/Index.cshtml.cs
+++ QForm/Pages/Index.cshtml.cs
@@ -5,9 +5,25 @@ namespace QForm.Pages
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        public string UserName { get; set; }
+        public string ResponseMessage { get; set; }
+
         public void OnGet()
         {
+            ResponseMessage = "アンケートにご協力ください。";
+        }
 
+        public void OnPost()
+        {
+            if (!string.IsNullOrEmpty(UserName))
+            {
+                ResponseMessage = $"{UserName}さん、回答ありがとうございました。";
+            }
+            else
+            {
+                ResponseMessage = "入力なし";
+            }
         }
     }
 }
